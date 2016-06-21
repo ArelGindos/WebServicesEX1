0859f302a5846bfbbc61bf1ca0a7cf89c672811e
ArelGindos <arel-g@hotmail.com>
2016-06-21 19:24:22 +0300

    added headers

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ mongoose.connection.once('open', function(){
 	});
 });
 
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	app.set('json spaces', 4);
+  	res.set("Content-Type", "application/json");
+  	next();
+});
+
 app.get('/getAllStudentsGrades', function(req,res){
 	res.status(200).json(ShenkarSchool.getAllStudentsGrades());
 });
